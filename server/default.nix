be90e51b9b86272db38857b0a54b43d2e6dc8a4c
xfeusw <khamrakulovkamron@gmail.com>
2026-02-08 16:31:31 +0500

    chore: removed internal flakes

--- server/default.nix
+++ server/default.nix
@@ -7,7 +7,6 @@
     };
   in
     import nixpkgs {overlays = [];},
-  ...
 }: let
   hlib = pkgs.haskell.lib;
   hpkgs = pkgs.haskell.packages."ghc910".override {
@@ -21,46 +20,9 @@
     };
   };
 in
-  pkgs.stdenv.mkDerivation {
-    name = "relago-server-dev";
-
-    nativeBuildInputs = with pkgs; [
-      git
-      nixd
-      statix
-      deadnix
-      alejandra
-    ];
-
-    # Runtime dependencies
-    buildInputs = [
-      hpkgs.cabal-install
-      hpkgs.cabal-add
-      hpkgs.haskell-language-server
-      hpkgs.fourmolu
-      hpkgs.hlint
-      hpkgs.hpack
-      hpkgs.cabal-fmt
-      hpkgs.postgresql-libpq
-      hpkgs.postgresql-libpq-configure
-
-      pkgs.just
-      pkgs.alejandra
-      pkgs.zlib
-      pkgs.treefmt
-      pkgs.libpq.dev
-      pkgs.zlib.dev
-      pkgs.postgresql
-      pkgs.libz
-      pkgs.libpq.pg_config
-      pkgs.pkg-config
-      pkgs.xz
-    ];
-
-    shellHook = ''
-      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${pkgs.postgresql}/lib
-    '';
-
-    # Environmental variables
-    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
-  }
+  hlib.overrideCabal (hpkgs.callCabal2nix "relago" ./. {}) (old: {
+    doCheck = true;
+    doHaddock = false;
+    enableLibraryProfiling = false;
+    enableExecutableProfiling = false;
+  })
